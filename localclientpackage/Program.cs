ca2e099053f471e12df208bb0295ddb53a63aefe
Ben Pearce <ben.pearce@octopus.com>
2017-12-01 09:52:31 +1000

    Added project pointing at local packages

---
+++ localclientpackage/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers;
+using Octopus.Client;
+using Octopus.Client.Extensibility;
+
+namespace localclientpackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = EnvironmentHelpers.GetServerUrl();
+            var apiKey = EnvironmentHelpers.GetApiKey();
+            var endpoint = new OctopusServerEndpoint(server, apiKey);
+            var repository = new OctopusRepository(endpoint);
+
+            var project = repository.Projects.GetAll().First();
+
+            KeyValuePair<string, Href> deploymentProcess = project.Links.Where(l => l.Key.Equals("DeploymentProcess")).FirstOrDefault();
+
+            ConsoleHelpers.Dump(deploymentProcess);
+
+            //return _octopus.Repository.Client.Get<ReleaseTemplateResource>(deploymentProcess.Value + "/template?channel=" + templateChannel.Id);
+
+            Console.ReadLine();
+        }
+    }
+}
