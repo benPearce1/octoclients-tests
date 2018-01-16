1bd8585d9e288e2fe8af02d89732f52d4c0df312
Ben Pearce <ben.pearce@octopus.com>
2018-01-16 13:46:06 +1000

    Added test project for double byte characters

---
+++ special-characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers;
+using Octopus.Client;
+
+namespace special_characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = EnvironmentHelpers.GetServerUrl();
+            var apiKey = EnvironmentHelpers.GetApiKey();
+            var endpoint = new OctopusServerEndpoint(server, apiKey);
+            var repository = new OctopusRepository(endpoint);
+
+            var tenantResources = repository.Tenants.FindByName("Team 😄");
+            ConsoleHelpers.Dump(tenantResources);
+
+            Console.ReadKey();
+        }
+    }
+}
